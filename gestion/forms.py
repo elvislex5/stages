186de1ddabd58c1fbab4cce9583f4aa3c9d52269
Elvislex5 <teurtysoyono@gmail.com>
2024-10-30 20:50:37 +0100

    First commit

--- gestion/forms.py
+++ gestion/forms.py
@@ -4,18 +4,17 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class CustomLoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Nom d\'utilisateur',
-    }))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Mot de passe',
-    }))
+    username = forms.CharField()
+    password = forms.CharField()
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(max_length=150, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
